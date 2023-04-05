c8ddf39b8c031d66ddaf95c6543225ee3e79b327
Apaala Chatterjee <apaala@gmail.com>
2023-04-05 15:39:17 -0400

    changes to wrapper to accept base path

--- illumina_wrapper.sh
+++ illumina_wrapper.sh
@@ -23,14 +23,16 @@ rscript=$1
 run_id=$2
 proj_id=$3
 outdir=$4
+basep=$5
 
 cmd1="rmarkdown::render('"
 cmd2="',params=list(runid='"
 cmd3="',projid='"
-cmd4="'),output_dir='"
-cmd5="',output_file=paste('"
-cmd6="','_',Sys.Date(),'_illumina_Report.pdf',sep=''))"
+cmd4="',basep='"
+cmd5="'),output_dir='"
+cmd6="',output_file=paste('"
+cmd7="','_',Sys.Date(),'_illumina_Report.pdf',sep=''))"
 
-$Rexec --vanilla -e $cmd1$rscript$cmd2$run_id$cmd3$proj_id$cmd4$outdir$cmd5$run_id$cell_id$cmd6
+$Rexec --vanilla -e $cmd1$rscript$cmd2$run_id$cmd3$proj_id$cmd4$basep$cmd5$outdir$cmd6$run_id$cell_id$cmd7
 
 echo "Report done"
